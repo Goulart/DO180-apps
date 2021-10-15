7a79bfa8215c6112f52bbe866ea7187f9714e182
Goulart <Goulart@users.noreply.github.com>
2021-10-15 10:02:00 +0100

    Incl. additional vars

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,18 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "<h1>Hello, World!</h1>"
+print "<p>php version is " . PHP_VERSION . "</p>\n";
+$svrip = $_SERVER['REMOTE_ADDR'];
+print "<p>Server IP=" . $srvip . "</p>\n";
+
+print "<p>User agent (browser) IP=" . $_SERVER['HTTP_USER_AGENT'] . "</p>\n";
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+print "<p>Client IP=" . $ip . "</p>\n";
+
 ?>
